d8a219d71b748a4f5e6251946704d7d9005577c6
Jiwon Bae <jwb8705@gmail.com>
2018-04-30 16:34:32 +0900

    [Codility] BinaryGap - 100%

--- src/main/java/Codility/BinaryGap.java
+++ src/main/java/Codility/BinaryGap.java
@@ -2,7 +2,24 @@ package Codility;
 
 class BinaryGap {
 /* runtime error */
+
     static int solution(int N) {
+        // write your code in Java SE 8
+        String binary = Integer.toBinaryString(N);
+        int max = 0;
+        int count = 0;
+        for(int i =1; i < binary.length(); i++){
+            if(binary.charAt(i) == '1'){
+                if(count > max) {max = count;}
+                count = 0;
+            }
+            if(binary.charAt(i) == '0'){
+                count++;
+            }
+        }
+        return max;
+    }
+    static int solution_runtimeError(int N) {
         String binary = Integer.toBinaryString(N);
         return getMaxGap(binary, 1, 0, 0);
     }
