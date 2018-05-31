80167c37e18018fa030b611da5df1734d66f3abd
Jiwon Bae <jwb8705@gmail.com>
2018-05-31 23:25:17 +0900

    [HackerRank] - IceCreamParlor

---
+++ src/main/java/Codility/TapeEquilibrium.java
@@ -0,0 +1,6 @@
+package Codility;
+
+public class TapeEquilibrium {
+
+
+}
---
+++ src/main/java/others/Memebox.java
@@ -0,0 +1,21 @@
+package others;
+
+/*
+A dominant member in the array is one that occupies over half the positions in the array, for example:
+
+{3, 67, 23, 67, 67}
+
+67 is a dominant member because it appears in the array in 3/5 (>50%) positions.
+
+Provide a method that takes in an array and returns an index of the dominant member if one exists and -1 if there is none.
+*/
+public class Memebox {
+    public int findDominant(int[] arr){
+        int count=0;
+        int max=0;
+        for(int i =0; i < arr.length; i++){
+            count++;
+            if
+        }
+    }
+}
